0384a8243a9c22db50fbc0203a5411c566f00b1e
Emily Brito <emilybtoliveira@gmail.com>
2022-10-12 09:50:16 -0300

    adiciona Bresenham para desenhar linhas

--- football 2.py
+++ football 2.py
@@ -24,8 +24,6 @@ last_axis_used = ""
 rotation_x_flag = 0
 rotation_y_flag = 0
 
-MOVE_UNIT = 1
-vel_unit = 1
 
 def init():
     glClearColor(0.0, 0.1, 0.0, 1.0) 
@@ -38,7 +36,6 @@ def init():
     gluPerspective(45, 640/640, 0.1, 80.0)
     glMatrixMode(GL_MODELVIEW)     
     
-
 def drawField():
     v1 = [ 10.0, 0.0, -5.0]
     v2 = [-10.0, 0.0, -5.0]
@@ -93,7 +90,7 @@ def drawGoalpost(x_pos):
     glTranslate(x_pos, 0.5, -5.0)
     cylinder = gluNewQuadric()
     gluQuadricDrawStyle (cylinder, GLU_LINE)
-    gluCylinder(cylinder, 0.4, 0.4, 3, 500, 500)
+    gluCylinder(cylinder, 0.15, 0.15, 3, 500, 500)
     """ glPopMatrix()
 
     glPushMatrix()
@@ -101,22 +98,22 @@ def drawGoalpost(x_pos):
     glTranslate(0, -3.5, 0.0)
     cylinder = gluNewQuadric()
     gluQuadricDrawStyle (cylinder, GLU_LINE)
-    gluCylinder(cylinder, 0.4, 0.4, 3, 100, 100)
+    gluCylinder(cylinder, 0.15, 0.15, 3, 100, 100)
     glPopMatrix()
 
     glPushMatrix()
     glRotate(180, 1, 0, 0)
-    glTranslate(x_pos, -5.0, -0.8)
+    glTranslate(x_pos, -5.0, -0.6)
     cylinder = gluNewQuadric()
     gluQuadricDrawStyle (cylinder, GLU_LINE)
-    gluCylinder(cylinder, 0.4, 0.4, 4.0, 100, 100)
+    gluCylinder(cylinder, 0.15, 0.15, 3.8, 100, 100)
     glPopMatrix()
 
 def drawBall():
     glColor3f(1.0, 1.0, 1.0)
    # glLoadIdentity()
     glPushMatrix()
-    glTranslatef (0, 2.7, 0.0)
+    glTranslatef (0, 2.5, 0.0)
     #glTranslatef (0.5, 0.0, 0.0)
     glRotatef(45, 0, 1, 0)
     glutWireSphere (0.5, 20, 20)
@@ -162,35 +159,91 @@ def bresenhamFieldLines(x1, y1, x2, y2, z1, z2, direction):
     #glVertex3f (9.0, 2.0, 2.5)
     glEnd()
 
+def circleSymmetry(xoffset, zoffset, x, z):
+    glVertex3f(xoffset+x, 2, zoffset+z);
+    glVertex3f(xoffset-x, 2, zoffset+z);
+    glVertex3f(xoffset+x, 2, zoffset-z);
+    glVertex3f(xoffset-x, 2, zoffset-z);
+    glVertex3f(xoffset+z, 2, zoffset+x);
+    glVertex3f(xoffset-z, 2, zoffset+x);
+    glVertex3f(xoffset+z, 2, zoffset-x);
+    glVertex3f(xoffset-z, 2, zoffset-x);
+
+#fonte do algoritmo: https://www.geeksforgeeks.org/bresenhams-circle-drawing-algorithm/
 def bresenhamFieldCircle(r):
     x = 0
-    y = r
-    d = 1 - r
+    z = r
+    d = 3 - 2 * r
 
     glBegin(GL_POINTS)
     glColor3f(1.0, 1.0, 1.0)
     
-    glVertex3f (x, y, 0)  
+    #glVertex3f (x, y, 0)
+    circleSymmetry(0, -1.5, x, z)
 
-    while x < r:
+    while x < z:
         if (d < 0): 
-            d +=2 * x + 3
+            d += (4 * x) + 0.06
             x += 0.01
         else:
-            d += 2 * (x - y) + 5
+            d += 4 * (x - z) + 0.010
             x += 0.01
-            #y -= 0.01        
+            z -= 0.01        
 
-        glVertex3f (x, y, 0)  
-        glVertex3f (-x, y, 0)  
-        glVertex3f (-x, -y, 0)  
-        glVertex3f (x, -y, 0)
-        print(x,y)
-
-    #print(y, x)
-         
+        circleSymmetry(0, -1.5, x, z)         
     glEnd()
 
+def bresenhamFieldHalfCircle(r, xoffset, zoffset):
+    x = 0
+    z = r
+    d = 3 - 2 * r
+
+    glBegin(GL_POINTS)
+    glColor3f(1.0, 1.0, 1.0)
+    glVertex3f(xoffset+x, 2, zoffset+z)
+    glVertex3f(xoffset-x, 2, zoffset+z)
+
+    while x < z:
+        if (d < 0): 
+            d += (4 * x) + 0.06
+            x += 0.01
+        else:
+            d += 4 * (x - z) + 0.010
+            x += 0.01
+            z -= 0.01        
+    
+        glVertex3f(xoffset+x, 2, zoffset+z)
+        glVertex3f(xoffset-x, 2, zoffset+z)   
+    glEnd()
+
+def drawFieldLines():
+    bresenhamFieldLines(-9.0, 2.0, 9.0, 2.0, 2.7, 2.7, "H")
+    bresenhamFieldLines(-9.0, 2.0, 9.0, 2.0, -5.5, -5.5, "H")
+    bresenhamFieldLines(-9.0, 2.0, 9.0, 2.0, 2.7, -5.5, "V")
+    bresenhamFieldLines(9.0, 2.0, 9.0, 2.0, 2.7, -5.5, "V")
+    bresenhamFieldLines(0, 2.0, 0, 2.0, 2.7, -5.5, "V")
+    
+    bresenhamFieldLines(-7.5, 2.0, 9.0, 2.0, 1.0, -3.5, "V")
+    bresenhamFieldLines(-9.0, 2.0, -7.5, 2.0, -3.5, -3.5, "H")
+    bresenhamFieldLines(-9.0, 2.0, -7.5, 2.0, 1.0, -3.5, "H")
+
+    bresenhamFieldLines(-6.5, 2.0, 9.0, 2.0, 2, -4.5, "V")
+    bresenhamFieldLines(-9.0, 2.0, -6.5, 2.0, -4.5, -5.5, "H")
+    bresenhamFieldLines(-9.0, 2.0, -6.5, 2.0, 2.0, -5.5, "H")
+
+    bresenhamFieldLines(7.5, 2.0, 9.0, 2.0, 1.0, -3.5, "V")
+    bresenhamFieldLines(7.5, 2.0, 9.0, 2.0, -3.5, -3.5, "H")
+    bresenhamFieldLines(7.5, 2.0, 9.0, 2.0, 1.0, -3.5, "H")
+
+    bresenhamFieldLines(6.5, 2.0, -9.0, 2.0, 2, -4.5, "V")
+    bresenhamFieldLines(6.5, 2.0, 9.0, 2.0, -4.5, -5.5, "H")
+    bresenhamFieldLines(6.5, 2.0, 9.0, 2.0, 2.0, -5.5, "H")
+
+    bresenhamFieldCircle(1.5)
+    glRotate(90,0,1,0)
+    bresenhamFieldHalfCircle(1.5, 1, -7.5)
+    glRotate(180,0,1,0)
+    bresenhamFieldHalfCircle(1.5, -1.0, -7.5)
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # Clear color and depth buffer
@@ -203,16 +256,10 @@ def display():
     drawBall()
 
     glTranslate(0, 0, 1.4)
-    drawGoalpost(8)
-    drawGoalpost(-8)
+    drawGoalpost(9.2)
+    drawGoalpost(-9.2)
 
-    bresenhamFieldLines(-9.0, 2.0, 9.0, 2.0, 2.5, 2.5, "H")
-    bresenhamFieldLines(-9.0, 2.0, 9.0, 2.0, -5.5, -5.5, "H")
-    bresenhamFieldLines(-9.0, 2.0, 9.0, 2.0, 2.5, -5.5, "V")
-    bresenhamFieldLines(9.0, 2.0, 9.0, 2.0, 2.5, -5.5, "V")
-    bresenhamFieldLines(0, 2.0, 0, 2.0, 2.5, -5.5, "V")
-
-    #bresenhamFieldCircle(1)
+    drawFieldLines()
 
     glutSwapBuffers()
 
@@ -224,79 +271,73 @@ def move_camera():
               lookat_x, lookat_y, lookat_z)
     print(f"posicao da camera: ({camera_x}, {camera_y}, {camera_z})")
     print(f"posicao do centro: ({center_x}, {center_y}, {center_z})")
-    #glMatrixMode(GL_PROJECTION) 
-    #glFrustum(-3.0, 3.0, 3.0, -3.0, 5.0, 15.0)
-    #gluPerspective(45, 640/640, 0.1, 80.0)
-
-def reshape(width, height): 
-    pass
 
 def keyboard_handler(key, x, y):
     global camera_x, camera_y, camera_z, center_x, center_y, center_z, lookat_x, lookat_y, lookat_z, drawing_rotation, rotation_x_flag, rotation_y_flag, last_y_drawing_rotation, last_x_drawing_rotation, last_axis_used
     
     if key == b'w': 
-        camera_y += MOVE_UNIT * vel_unit
-        center_y += MOVE_UNIT * vel_unit
+        camera_y += 1
+        center_y += 1
 
     elif key == b's':
-        camera_y -= MOVE_UNIT * vel_unit
-        center_y -= MOVE_UNIT * vel_unit
+        camera_y -= 1
+        center_y -= 1
 
     elif key == b'a': 
-        camera_x -= MOVE_UNIT * vel_unit
-        center_x -= MOVE_UNIT * vel_unit
+        camera_x -= 1
+        center_x -= 1
 
     elif key == b'd':
-        camera_x += MOVE_UNIT * vel_unit
-        center_x += MOVE_UNIT * vel_unit
+        camera_x += 1
+        center_x += 1
 
     elif key == b'q': 
-        camera_z -= MOVE_UNIT * vel_unit
-        center_z -= MOVE_UNIT * vel_unit
+        camera_z -= 1
+        center_z -= 1
 
     elif key == b'e':
-        camera_z += MOVE_UNIT * vel_unit
-        center_z += MOVE_UNIT * vel_unit
+        camera_z += 1
+        center_z += 1
 
     elif key == b'm':
-        #lookat_y -= MOVE_UNIT * 1
+        #lookat_y -= 1 * 1
 
         if last_axis_used == "x":
             last_x_drawing_rotation = drawing_rotation
             drawing_rotation = last_y_drawing_rotation
 
-        drawing_rotation -= MOVE_UNIT
+        drawing_rotation -= 1
         rotation_x_flag = 0
         rotation_y_flag = 1
         last_axis_used = "y"
 
     elif key == b'b':
-       # lookat_x += MOVE_UNIT * 0.1
+       # lookat_x += 1 * 0.1
         if last_axis_used == "x":
             last_x_drawing_rotation = drawing_rotation
             drawing_rotation = last_y_drawing_rotation
-        drawing_rotation += MOVE_UNIT
+        drawing_rotation += 1
         rotation_x_flag = 0
         rotation_y_flag = 1
         last_axis_used = "y"
 
 
     elif key == b'n':
-       # lookat_x += MOVE_UNIT * 0.1
+       # lookat_x += 1 * 0.1
         if last_axis_used == "y":
             last_y_drawing_rotation = drawing_rotation
             drawing_rotation = last_x_drawing_rotation
-        drawing_rotation -= MOVE_UNIT
+        drawing_rotation -= 1
         rotation_x_flag = 1
         rotation_y_flag = 0
         last_axis_used = "x"
     
     elif key == b'j':
-       # lookat_x += MOVE_UNIT * 0.1
+       # lookat_x += 1 * 0.1
         if last_axis_used == "y":
             last_y_drawing_rotation = drawing_rotation
             drawing_rotation = last_x_drawing_rotation  
-        drawing_rotation += MOVE_UNIT
+        drawing_rotation += 1
         rotation_x_flag = 1
         rotation_y_flag = 0
         last_axis_used = "x"
@@ -311,7 +352,6 @@ glutInitWindowSize(900, 600)
 glutCreateWindow("Football Field Simulator")
 init() 
 glutDisplayFunc(display)
-#glutReshapeFunc(reshape)
 glutKeyboardFunc(keyboard_handler)
 
 glutMainLoop()
